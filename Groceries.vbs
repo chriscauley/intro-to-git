9506e3013766b52f385d243e629f89665a22a472
JohnMorgan302 <johnmorgan302@gmail.com>
2015-02-17 19:02:29 -0600

    Grocery Getter Code
    Scripts in VBS and Widnows Command Shell to demo how GIT works with
    these languages.

---
+++ Groceries.vbs
@@ -0,0 +1,2 @@
+msgbox "Don't forget the potato chips"
+msgbox "Or the beer!"
---
+++ cart.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo =[ Grocery List ]==============
+echo 1) Self Rising Flower
+echo 2) Beer
+echo 3) Sugar
